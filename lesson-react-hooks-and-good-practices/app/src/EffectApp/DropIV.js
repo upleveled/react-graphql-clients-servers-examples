cd7b7fc678ee2f2865381bba47f7db742b59c419
Karl Horky <karl.horky@gmail.com>
2022-03-04 18:30:28 +0100

    Fix linting warning

--- lesson-react-hooks-and-good-practices/app/src/EffectApp/DropIV.js
+++ lesson-react-hooks-and-good-practices/app/src/EffectApp/DropIV.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  console.log("render Drop");
-  const [isOnline, setOnline] = useState(true);
+  console.log('render Drop');
+  const [isOnline, setIsOnline] = useState(true);
 
   function onOffline() {
-    setOnline(false);
+    setIsOnline(false);
   }
   function onOnline() {
-    setOnline(true);
+    setIsOnline(true);
   }
   useEffect(() => {
-    console.log("useEffect Drop");
-    window.addEventListener("online", onOnline);
-    window.addEventListener("offline", onOffline);
+    console.log('useEffect Drop');
+    window.addEventListener('online', onOnline);
+    window.addEventListener('offline', onOffline);
 
     return () => {
-      console.log("cleanup useEffect Drop");
-      window.removeEventListener("online", onOnline);
-      window.removeEventListener("offline", onOffline);
+      console.log('cleanup useEffect Drop');
+      window.removeEventListener('online', onOnline);
+      window.removeEventListener('offline', onOffline);
     };
   }, []);
 
-  return <div>{isOnline ? "Online" : "Offline"}</div>;
+  return <div>{isOnline ? 'Online' : 'Offline'}</div>;
 }
 
 export default App;

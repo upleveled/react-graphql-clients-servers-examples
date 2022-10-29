05d70e22fadefa3d5782fdb547654461fd444f08
Karl Horky <karl.horky@gmail.com>
2022-10-29 15:21:40 +0200

    Skip type checking and linting during build

--- packages/graphql-servers-todos/next.config.js
+++ packages/graphql-servers-todos/next.config.js
@@ -1,7 +1,16 @@
-module.exports = /** @type {import("next").NextConfig} */ ({
+/** @type {import("next").NextConfig} */
+const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config, options) => {
     config.experiments = config.experiments || {};
     config.experiments.topLevelAwait = true;
     return config;
   },
-});
+};
+
+module.exports = config;
